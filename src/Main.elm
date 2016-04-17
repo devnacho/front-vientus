364b4e3803b5b13278c8d4b3cf4186562223f242
jgnatch <nachojgutierrez@gmail.com>
2016-04-17 14:07:34 +0200

    set windSpeed

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,8 @@ module Main (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, targetValue, on)
+import String exposing (toInt)
 import Effects exposing (Effects)
 import StartApp
 
@@ -35,7 +36,16 @@ port swap : Signal.Signal Bool
 
 
 type alias Model =
-  Int
+  { email : String
+  , windSpeed : String
+  }
+
+
+initalModel : Model
+initalModel =
+  { email = ""
+  , windSpeed = "11"
+  }
 
 
 
@@ -43,7 +53,7 @@ type alias Model =
 
 
 init =
-  ( 0, Effects.none )
+  ( initalModel, Effects.none )
 
 
 view address model =
@@ -55,7 +65,14 @@ view address model =
         [ div
             [ class "form-field" ]
             [ label [] [ text "Email" ]
-            , input [ id "username-field", type' "text" ] []
+            , input
+                [ id "username-field"
+                , type' "text"
+                , placeholder "your-email@gmail.com"
+                , on "input" targetValue (Signal.message address << SetEmail)
+                , value model.email
+                ]
+                []
             ]
         , div
             [ class "form-field" ]
@@ -69,7 +86,12 @@ view address model =
         , div
             [ class "form-field" ]
             [ label [] [ text "Minimum Wind Speed (in knots)" ]
-            , input [ type' "number" ] []
+            , input
+                [ type' "number"
+                , value model.windSpeed
+                , on "input" targetValue (Signal.message address << SetWindSpeed)
+                ]
+                []
             ]
         ]
     ]
@@ -81,7 +103,8 @@ view address model =
 
 type Action
   = NoOp
-  | Increment
+  | SetEmail String
+  | SetWindSpeed String
 
 
 update action model =
@@ -89,5 +112,16 @@ update action model =
     NoOp ->
       ( model, Effects.none )
 
-    Increment ->
-      ( model + 1, Effects.none )
+    SetEmail str ->
+      ( { model
+          | email = str
+        }
+      , Effects.none
+      )
+
+    SetWindSpeed str ->
+      ( { model
+          | windSpeed = str
+        }
+      , Effects.none
+      )
