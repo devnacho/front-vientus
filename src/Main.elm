e3343dfdb530162e348551242fd949a8dce0b771
jgnatch <nachojgutierrez@gmail.com>
2016-04-17 12:50:28 +0200

    pruning

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,8 @@ import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
 import Effects exposing (Effects)
 
--- official 'Elm Architecture' package
--- https://github.com/evancz/start-app
 import StartApp
 
--- component import example
-import Components.Hello exposing ( hello )
 
 -- APP KICK OFF!
 app = StartApp.start
@@ -30,16 +26,10 @@ type alias Model = Int
 init =
   (0, Effects.none)
 
--- VIEW
--- Examples of:
--- 1)  an externally defined component ('hello', takes 'model' as arg)
--- 2a) styling through CSS classes (external stylesheet)
--- 2b) styling using inline style attribute (two variants)
 view address model =
   div
     [ class "mt-palette-accent", style styles.wrapper ]
-    [ hello model
-    ,  p [ style [( "color", "#FFF")] ] [ text ( "Elm Webpack Starter" ) ]
+    [  p [ style [( "color", "#FFF")] ] [ text ( "Elm Webpack Starter" ) ]
     ,  button [ class "mt-button-sm", onClick address Increment ] [ text "FTW!" ]
     ,  img [ src "img/elm.jpg", style [( "display", "block"), ( "margin", "10px auto")] ] []
     ]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // pull in desired CSS/SASS files
-require( './styles/mt.css/mt.scss' );
-require( './styles/app.css' );
+//require( './styles/app.css' );
 
 var Elm = require( './Main' );
 Elm.embed( Elm.Main, document.getElementById( 'main' ), { swap: false } );
